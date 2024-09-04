02fff846e24f9fc3f0ce4696fdb72a4d8f5013f4
Gabriel Depiro <gabriel.depiro9@gmail.com>
2024-09-04 12:43:38 -0300

    Imersão - dia 03

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-console.log("Brasil");
+console.log(paises)
---
+++ dados.js
@@ -0,0 +1,51 @@
+let paises =[{
+    titulo: "Brasil - República Federativa do Brasil",
+    continente: "América do Sul",
+    capital: "Brasília",
+    idiomaOficial: "Português",
+    descricao: "O Brasil, o maior país da América do Sul, é conhecido por sua rica biodiversidade, cultura vibrante e paisagens deslumbrantes. Com uma costa extensa banhada pelo Oceano Atlântico, o país abriga a maior floresta tropical do mundo, a Amazônia, além de outros biomas como o Cerrado e a Mata Atlântica. A cultura brasileira é uma mistura de influências indígenas, africanas e europeias, resultando em uma rica diversidade musical, culinária e artística.",
+    link: "https://pt.wikipedia.org/wiki/Brasil"
+}, {
+    titulo: "Canadá",
+    continente: "América do Norte",
+    capital: "Capital: Ottawa",
+    idiomaOficial: "Inglês e Francês",
+    descricao: "O Canadá, o segundo maior país do mundo em área, é famoso por suas paisagens naturais exuberantes, como as Montanhas Rochosas e as florestas boreais. É um país multicultural, com uma população diversificada que reflete sua história de imigração. O Canadá é conhecido por sua alta qualidade de vida, sistema de saúde universal e cidades modernas como Toronto e Vancouver.",
+    link: "https://pt.wikipedia.org/wiki/Canad%C3%A1"
+}, {
+    titulo: "Japão",
+    continente: "Ásia",
+    capital: "Tóquio",
+    idiomaOficial:"Japonês",
+    descricao: "O Japão, um arquipélago localizado no Oceano Pacífico, é um país com uma longa e rica história e cultura. Famoso por sua tecnologia avançada, tradições milenares, como o chá e o sushi, e paisagens montanhosas, o Japão é uma das maiores economias do mundo. As cidades japonesas, como Tóquio e Kyoto, são conhecidas por sua arquitetura moderna e tradicional.",
+    link: "https://pt.wikipedia.org/wiki/Jap%C3%A3o"
+},{
+    titulo: "França - República Francesa",
+    continente: "Europa",
+    capital: "Paris",
+    idiomaOficial: "Francês",
+    descricao: "A França é conhecida por sua rica história, cultura e gastronomia. Paris, a capital, é um dos destinos turísticos mais populares do mundo.",
+    link: "https://pt.wikipedia.org/wiki/França"
+},{
+    titulo: "Austrália - Commonwealth da Austrália",
+    continente: "Oceania",
+    capital: "Canberra",
+    idiomaOficial: "Inglês",
+    descricao: "A Austrália é um país insular conhecido por sua fauna única, como o canguru e o coala. Sydney e Melbourne são suas maiores cidades.",
+    link: "https://pt.wikipedia.org/wiki/Austrália"
+},{
+    titulo: "Egito - República Árabe do Egito",
+    continente: "África",
+    capital: "Cairo",
+    idiomaOficial: "Árabe",
+    descricao: "O Egito é um país com uma rica história, berço de uma das civilizações mais antigas do mundo. As pirâmides de Gizé e o rio Nilo são alguns dos seus principais atrativos turísticos.",
+    link: "https://pt.wikipedia.org/wiki/Egito"
+},
+{
+    titulo: "Antártida",
+    continente: "Antártida",
+    capital: "Não possui",
+    idiomaOficial: "Vários", // A Antártida não tem um idioma oficial único
+    descricao: "A Antártida é o continente mais frio e seco da Terra, coberto por uma espessa camada de gelo. É um continente dedicado à pesquisa científica.",
+    link: "https://pt.wikipedia.org/wiki/Antártida"
+}];
