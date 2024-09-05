0b4a0741f0af8a5fe59c4bc25456a29007598c0e
Gabriel Depiro <gabriel.depiro9@gmail.com>
2024-09-05 13:22:31 -0300

    Imersão - dia 04

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log(paises)
+/*
+  Esta função é responsável por realizar uma pesquisa em uma lista de países e exibir os resultados em uma seção HTML.
+  Ela itera sobre cada país, cria um elemento HTML para cada um e adiciona os resultados à seção.
+*/
+function pesquisar(){
+    // Seleciona a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa")
+
+    // String para armazenar os resultados da pesquisa
+    let resultados = ""
+
+    // Itera sobre os dados dos países
+    for(let dado of paises){
+        // Cria uma div para cada país com as informações relevantes
+        resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank" text-align: center>${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta"> • ${dado.continente}</p>
+                <p class="descricao-meta"> • ${dado.capital}</p>    
+                <p class="descricao-meta"> • ${dado.idiomaOficial}</p>
+                <p class="descricao-meta"> • ${dado.moedaOficial}</p>   
+                <p class="descricao-meta">${dado.descricao}.</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+        `;
+    }
+    // Atualiza a seção HTML com os resultados da pesquisa
+    section.innerHTML = resultados
+}
+
+
+
--- dados.js
+++ dados.js
@@ -3,6 +3,7 @@ let paises =[{
     continente: "América do Sul",
     capital: "Brasília",
     idiomaOficial: "Português",
+    moedaOficial: "Real(BRL)",
     descricao: "O Brasil, o maior país da América do Sul, é conhecido por sua rica biodiversidade, cultura vibrante e paisagens deslumbrantes. Com uma costa extensa banhada pelo Oceano Atlântico, o país abriga a maior floresta tropical do mundo, a Amazônia, além de outros biomas como o Cerrado e a Mata Atlântica. A cultura brasileira é uma mistura de influências indígenas, africanas e europeias, resultando em uma rica diversidade musical, culinária e artística.",
     link: "https://pt.wikipedia.org/wiki/Brasil"
 }, {
@@ -10,6 +11,7 @@ let paises =[{
     continente: "América do Norte",
     capital: "Capital: Ottawa",
     idiomaOficial: "Inglês e Francês",
+    moedaOficial: "Dólar Canadense(CAD)",
     descricao: "O Canadá, o segundo maior país do mundo em área, é famoso por suas paisagens naturais exuberantes, como as Montanhas Rochosas e as florestas boreais. É um país multicultural, com uma população diversificada que reflete sua história de imigração. O Canadá é conhecido por sua alta qualidade de vida, sistema de saúde universal e cidades modernas como Toronto e Vancouver.",
     link: "https://pt.wikipedia.org/wiki/Canad%C3%A1"
 }, {
@@ -17,6 +19,7 @@ let paises =[{
     continente: "Ásia",
     capital: "Tóquio",
     idiomaOficial:"Japonês",
+    moedaOficial: "Iene(JPY)",
     descricao: "O Japão, um arquipélago localizado no Oceano Pacífico, é um país com uma longa e rica história e cultura. Famoso por sua tecnologia avançada, tradições milenares, como o chá e o sushi, e paisagens montanhosas, o Japão é uma das maiores economias do mundo. As cidades japonesas, como Tóquio e Kyoto, são conhecidas por sua arquitetura moderna e tradicional.",
     link: "https://pt.wikipedia.org/wiki/Jap%C3%A3o"
 },{
@@ -24,6 +27,7 @@ let paises =[{
     continente: "Europa",
     capital: "Paris",
     idiomaOficial: "Francês",
+    moedaOficial: "Euro(€)",
     descricao: "A França é conhecida por sua rica história, cultura e gastronomia. Paris, a capital, é um dos destinos turísticos mais populares do mundo.",
     link: "https://pt.wikipedia.org/wiki/França"
 },{
@@ -31,6 +35,7 @@ let paises =[{
     continente: "Oceania",
     capital: "Canberra",
     idiomaOficial: "Inglês",
+    moedaOficial: "Dólar Australiano(AUD)",
     descricao: "A Austrália é um país insular conhecido por sua fauna única, como o canguru e o coala. Sydney e Melbourne são suas maiores cidades.",
     link: "https://pt.wikipedia.org/wiki/Austrália"
 },{
@@ -38,6 +43,7 @@ let paises =[{
     continente: "África",
     capital: "Cairo",
     idiomaOficial: "Árabe",
+    moedaOficial: "Libra Egípcia(EGP)",
     descricao: "O Egito é um país com uma rica história, berço de uma das civilizações mais antigas do mundo. As pirâmides de Gizé e o rio Nilo são alguns dos seus principais atrativos turísticos.",
     link: "https://pt.wikipedia.org/wiki/Egito"
 },
@@ -46,6 +52,7 @@ let paises =[{
     continente: "Antártida",
     capital: "Não possui",
     idiomaOficial: "Vários", // A Antártida não tem um idioma oficial único
+    moedaOficial:"Não possui um governo centralizado e, consequentemente, não tem uma moeda oficial.",
     descricao: "A Antártida é o continente mais frio e seco da Terra, coberto por uma espessa camada de gelo. É um continente dedicado à pesquisa científica.",
     link: "https://pt.wikipedia.org/wiki/Antártida"
 }];
