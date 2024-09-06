1070346975b1c884d268dda2f031d41868d5722b
Gabriel Depiro <gabriel.depiro9@gmail.com>
2024-09-06 11:37:36 -0300

    Imersão - dia 05

--- app.js
+++ app.js
@@ -6,26 +6,49 @@ function pesquisar(){
     // Seleciona a seção onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa")
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if(!campoPesquisa.trim()){
+        section.innerHTML = "<p>Nada foi encontrado.</p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
     // String para armazenar os resultados da pesquisa
-    let resultados = ""
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     // Itera sobre os dados dos países
     for(let dado of paises){
-        // Cria uma div para cada país com as informações relevantes
-        resultados += `
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            // Cria uma div para cada país com as informações relevantes
+            resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank" text-align: center>${dado.titulo}</a>
                 </h2>
                 <p class="descricao-meta"> • ${dado.continente}</p>
-                <p class="descricao-meta"> • ${dado.capital}</p>    
+                <p class="descricao-meta"> • ${dado.capital}</p>  
                 <p class="descricao-meta"> • ${dado.idiomaOficial}</p>
                 <p class="descricao-meta"> • ${dado.moedaOficial}</p>   
                 <p class="descricao-meta">${dado.descricao}.</p>
                 <a href=${dado.link} target="_blank">Mais informações</a>
             </div>
-        `;
+            `;
+        
+        } 
     }
+
+    if (!resultados){
+            resultados = "<p>Não foi encontrado nenhum resultado com sua pesquisa</p>"
+    }
+
     // Atualiza a seção HTML com os resultados da pesquisa
     section.innerHTML = resultados
 }
--- dados.js
+++ dados.js
@@ -5,7 +5,8 @@ let paises =[{
     idiomaOficial: "Português",
     moedaOficial: "Real(BRL)",
     descricao: "O Brasil, o maior país da América do Sul, é conhecido por sua rica biodiversidade, cultura vibrante e paisagens deslumbrantes. Com uma costa extensa banhada pelo Oceano Atlântico, o país abriga a maior floresta tropical do mundo, a Amazônia, além de outros biomas como o Cerrado e a Mata Atlântica. A cultura brasileira é uma mistura de influências indígenas, africanas e europeias, resultando em uma rica diversidade musical, culinária e artística.",
-    link: "https://pt.wikipedia.org/wiki/Brasil"
+    link: "https://pt.wikipedia.org/wiki/Brasil",
+    tags:"Rio de Janeiro São Paulo Sao Paulo Futebol Carnaval Amazônia Amazonia Salvador"
 }, {
     titulo: "Canadá",
     continente: "América do Norte",
@@ -13,7 +14,8 @@ let paises =[{
     idiomaOficial: "Inglês e Francês",
     moedaOficial: "Dólar Canadense(CAD)",
     descricao: "O Canadá, o segundo maior país do mundo em área, é famoso por suas paisagens naturais exuberantes, como as Montanhas Rochosas e as florestas boreais. É um país multicultural, com uma população diversificada que reflete sua história de imigração. O Canadá é conhecido por sua alta qualidade de vida, sistema de saúde universal e cidades modernas como Toronto e Vancouver.",
-    link: "https://pt.wikipedia.org/wiki/Canad%C3%A1"
+    link: "https://pt.wikipedia.org/wiki/Canad%C3%A1",
+    tags: "Toronto Montreal Quebec Folhas Outono"
 }, {
     titulo: "Japão",
     continente: "Ásia",
@@ -21,7 +23,8 @@ let paises =[{
     idiomaOficial:"Japonês",
     moedaOficial: "Iene(JPY)",
     descricao: "O Japão, um arquipélago localizado no Oceano Pacífico, é um país com uma longa e rica história e cultura. Famoso por sua tecnologia avançada, tradições milenares, como o chá e o sushi, e paisagens montanhosas, o Japão é uma das maiores economias do mundo. As cidades japonesas, como Tóquio e Kyoto, são conhecidas por sua arquitetura moderna e tradicional.",
-    link: "https://pt.wikipedia.org/wiki/Jap%C3%A3o"
+    link: "https://pt.wikipedia.org/wiki/Jap%C3%A3o",
+    tags: "Anime Mangá Manga"
 },{
     titulo: "França - República Francesa",
     continente: "Europa",
@@ -29,7 +32,8 @@ let paises =[{
     idiomaOficial: "Francês",
     moedaOficial: "Euro(€)",
     descricao: "A França é conhecida por sua rica história, cultura e gastronomia. Paris, a capital, é um dos destinos turísticos mais populares do mundo.",
-    link: "https://pt.wikipedia.org/wiki/França"
+    link: "https://pt.wikipedia.org/wiki/França",
+    tags: "Torre Eifel"
 },{
     titulo: "Austrália - Commonwealth da Austrália",
     continente: "Oceania",
@@ -37,7 +41,8 @@ let paises =[{
     idiomaOficial: "Inglês",
     moedaOficial: "Dólar Australiano(AUD)",
     descricao: "A Austrália é um país insular conhecido por sua fauna única, como o canguru e o coala. Sydney e Melbourne são suas maiores cidades.",
-    link: "https://pt.wikipedia.org/wiki/Austrália"
+    link: "https://pt.wikipedia.org/wiki/Austrália",
+    tags: "canguru Sydney Mebourne"
 },{
     titulo: "Egito - República Árabe do Egito",
     continente: "África",
@@ -45,7 +50,8 @@ let paises =[{
     idiomaOficial: "Árabe",
     moedaOficial: "Libra Egípcia(EGP)",
     descricao: "O Egito é um país com uma rica história, berço de uma das civilizações mais antigas do mundo. As pirâmides de Gizé e o rio Nilo são alguns dos seus principais atrativos turísticos.",
-    link: "https://pt.wikipedia.org/wiki/Egito"
+    link: "https://pt.wikipedia.org/wiki/Egito",
+    tags: "Pirâmides Piramides Nilo Faraós Faraos Antiguidade"
 },
 {
     titulo: "Antártida",
@@ -54,5 +60,6 @@ let paises =[{
     idiomaOficial: "Vários", // A Antártida não tem um idioma oficial único
     moedaOficial:"Não possui um governo centralizado e, consequentemente, não tem uma moeda oficial.",
     descricao: "A Antártida é o continente mais frio e seco da Terra, coberto por uma espessa camada de gelo. É um continente dedicado à pesquisa científica.",
-    link: "https://pt.wikipedia.org/wiki/Antártida"
+    link: "https://pt.wikipedia.org/wiki/Antártida",
+    tags: "Pinguins pesquisa científica cientifica"
 }];
